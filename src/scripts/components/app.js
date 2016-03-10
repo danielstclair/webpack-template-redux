6b46346aa5c43c5fe1966881dc8125d06a6e9c30
DANIEL A. ST CLAIR <danielstclair@us.ibm.com>
2016-03-10 10:25:02 -0600

    finished boilerplate app structure and installed dependencies.

---
+++ src/scripts/components/app.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return(
+      <div>Welcome. Let's get started with redux.</div>
+    )
+  }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 import '../styles/main.scss';
-import '../../index.html';
 
-console.log(`it's working with es6 and stuff`);
-// console.log(sugar);
+// boilerplate imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleare = applyMiddleware(ReduxPromise)(createStore);
+
+// Must refactor once using redux
+
+ReactDOM.render(<App />, document.querySelector('.container'));
